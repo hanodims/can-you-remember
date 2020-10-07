c1d099426b43e09a343169e68839a5f1a3df0e95
hanodims <hend--mohammed@outlook.com>
2020-10-07 20:11:57 +0300

    on click the cards flip

--- src/App.js
+++ src/App.js
@@ -10,10 +10,12 @@ import cards from "./data";
 //components
 import Cards from "./components/Cards";
 
+
 function App() {
   let cardsGrid = cards.map(card => (
-    <Cards key={card.id} card={card} />
-  ));
+  <Cards key={card.id} card={card} />
+  )
+    );
   return <div className="App border my-5">
   <Jumbotron className="mt-5 ml-5 mr-5">
     <h1>Can you remember?</h1>
@@ -34,7 +36,7 @@ function App() {
       </Row>
     </Container>
   </div>
-  );
+  
 
   </div>;
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,26 +1,39 @@
-import React from "react";
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
+import React, { useState } from "react";
+import ReactCardFlip from "react-card-flip";
 import Col from 'react-bootstrap/Col'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../App.css";
-
 // Card Back Image
 import cardBack from "../images/CardBack.jpg";
 
-const Cards = ({ card }) => {
+
+const Cards = ({ card, checkFlipped }) => {
+  const [flipped, changeFlip] = useState(false);
+  const handleFlip = () => {
+    changeFlip(!flipped);
+  };
+
     return (
       <Col className="col-3 my-1">
-      <img
-        className="mx-auto"
-        src={card.front}
-        //   used percentages instead of pixels to be responsive with the screen size
-        height="100%"
-        width="100%"
-        key="front"
-      />
-    </Col>
+      <ReactCardFlip isFlipped={flipped} flipDirection="horizontal">
+        <img
+          className="mx-auto"
+          src={cardBack}
+          height="100%"
+          width="100%"
+          key="front"
+          onClick={() => handleFlip()}
+        />
+        <img
+          className="mx-auto"
+          src={card.front}
+          height="100%"
+          width="100%"
+          key="back"
+          onClick={() => handleFlip()}
+        />
+      </ReactCardFlip>
+      </Col>
     )};
   
   export default Cards;
