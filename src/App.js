a9b36ecbe10f9b71718c90820bf052d24227ae6b
hanodims <hend--mohammed@outlook.com>
2020-10-07 20:51:50 +0300

    Done with randomizing card behavior

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {useState} from "react";
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
@@ -6,16 +6,20 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 //Data
-import cards from "./data";
+import allCards from "./data";
 //components
 import Cards from "./components/Cards";
+// Utils
+import { shuffle } from "./utils";
 
 
 function App() {
-  let cardsGrid = cards.map(card => (
-  <Cards key={card.id} card={card} />
-  )
-    );
+  const [cards, setCards] = useState(shuffle([...allCards, ...allCards]));
+
+  const cardsGrid = cards.map((card, idx) => (
+    <Cards key={`${card.id}-${idx}`} card={card} />
+  ));
+  
   return <div className="App border my-5">
   <Jumbotron className="mt-5 ml-5 mr-5">
     <h1>Can you remember?</h1>
